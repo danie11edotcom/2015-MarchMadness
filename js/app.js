defc4504d5ea78df8349ae606831edea7ea3dec6
Danielle <danie11edotcom@users.noreply.github.com>
2015-08-20 21:08:12 -0400

    Added var to width and height variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   
-  var margin = {top: 20, right: 120, bottom: 20, left: 120};
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
       width = 960 - margin.right - margin.left;
       height = 800 - margin.top - margin.bottom;
 
